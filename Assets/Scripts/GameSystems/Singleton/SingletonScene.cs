860f4dec1e98c36efac7e5975e352ba51ec5f69c
Óscar López <90772105+OscarLM32@users.noreply.github.com>
2023-11-16 12:26:28 +0100

    Some files rearrangement

---
+++ Assets/Scripts/GameSystems/Singleton/SingletonScene.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace GameSystems.Singleton
+{
+    public abstract class SingletonScene<T> : MonoBehaviour where T : Component
+    {
+        protected static T i = null;
+
+        public static T I
+        {
+            get
+            {
+                if (i == null)
+                {
+                    Debug.LogError("The singleton trying to be accessed cannot be found");
+                }
+                return i;
+            }
+            private set
+            {
+                i = value;
+            }
+        }
+
+        protected void Awake()
+        {
+            if (i != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                I = this as T;
+            }
+            OnAwake();
+        }
+
+        protected abstract void OnAwake();
+
+    }
+}
