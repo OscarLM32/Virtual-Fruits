2613e00ba0f862f46fb8b422757a45552df61b8c
Óscar López <oscar_lm_geminis@hotmail.es>
2024-05-20 13:22:57 +0200

    Main menu done
    missing some refinement when it comes to the visual aspect

--- Assets/Scripts/Menus/MainMenu/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu/MainMenu.cs
@@ -2,46 +2,30 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+namespace Menus.MainMenu
 {
-    public GameObject MainMenuOptions;
-    public TextMeshProUGUI Score;
-
-    private AudioManager _audioManager;
-    private void Start()
+    public class MainMenu : MonoBehaviour
     {
-        _audioManager = GetComponent<AudioManager>();
-        SetScore();
-    }
 
-    public void OpenMenu(GameObject menu)
-    {
-        _audioManager.Play("Button");
-        MainMenuOptions.SetActive(false);
-        menu.SetActive(true);
-    }
+        public void OpenMenu(GameObject menu)
+        {
+            menu.SetActive(true);
+        }
 
-    public void CloseMenu(GameObject menu)
-    {
-        _audioManager.Play("Button");
-        menu.SetActive(false);
-        MainMenuOptions.SetActive(true);
-    }
+        public void CloseMenu(GameObject menu)
+        {
+            menu.SetActive(false);
+        }
 
-    public void Play()
-    {
-        _audioManager.Play("Button");
-        SceneManager.LoadScene(1);
-    }
+        public void Play()
+        {
+            SceneManager.LoadScene(1);
+        }
 
-    public void CloseApp()
-    {
-        Application.Quit();
-    }
+        public void CloseApp()
+        {
+            Application.Quit();
+        }
 
-    private void SetScore()
-    {
-        Score.text = SaveLoadSystem.I.GetTotalPickedFruits().ToString();
     }
-
 }
