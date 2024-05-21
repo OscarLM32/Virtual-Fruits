1d56c72a7c9e9f21c93d059cf844e678cac434c1
Óscar López <oscar_lm_geminis@hotmail.es>
2024-05-21 14:20:34 +0200

    Initial class behavior and structure

--- Assets/Scripts/Level/FloatingPlatform.cs
+++ Assets/Scripts/Level/FloatingPlatform.cs
@@ -1,18 +1,49 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FloatingPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool _platformFalling;
+    private float _floatingTime = 0.75f;
+    private float _timeAfterMotorStopToFall = 0.5f;
+
+    private float _respawnTime = 3f;
+
+    private Rigidbody2D _rb;
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        //Set up dyamic difficulty settings
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (_platformFalling) return;
+
+        _platformFalling = true;
+        StartCoroutine(Fall());
+    }
+
+
+    private IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(_floatingTime);
+        //Stop animation
+        yield return new WaitForSeconds(_timeAfterMotorStopToFall);
+        //Unfreeze position Y
+    }
+
+    private IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(_respawnTime);
+        //Move platform to original position
+        //Lock Y position and set gravity
+        //Play floating animation
+    }
+
 }
